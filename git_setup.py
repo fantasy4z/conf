32b78efac072570a1728b39cd7f91d74f5b3605f
Guang-De Lin <fs.in.nccu@gmail.com>
2015-09-30 15:16:24 +0800

    New python script to setup git configs.

---
+++ git_setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os
+import subprocess
+
+def getGitConfigs():
+    return [
+        ['alias.st', 'status'],
+        ['alias.co', 'checkout'],
+        ['alias.d', 'difftool'],
+        ['diff.tool', 'vimdiff'],
+        ['difftool.prompt', 'false'],
+        ['push.default', 'simple'],
+        ['user.name', 'Guang-De Lin'],
+        ['user.email', 'fs.in.nccu@gmail.com'],
+    ]
+
+def main():
+    devNull = open(os.devnull, 'w')
+    ret = subprocess.call(['which', 'git'], stdout = devNull, stderr = subprocess.STDOUT)
+    devNull.close()
+    if ret != 0:
+        print("Cannot locate git executable.")
+        return 1
+
+    for config in getGitConfigs():
+        callArgs = ['git', 'config', '--global']
+        callArgs.extend(config)
+        subprocess.call(callArgs);
+
+if __name__ == '__main__':
+    main()
+
